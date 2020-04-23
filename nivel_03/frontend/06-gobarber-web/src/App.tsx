c4fb932ea50709a5af2772be09e54a0dd71797f9
virb30 <virb30@gmail.com>
2020-04-23 11:50:00 -0300

    finishing application - class completed

--- nivel_03/frontend/06-gobarber-web/src/App.tsx
+++ nivel_03/frontend/06-gobarber-web/src/App.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import SignIn from './pages/SignIn';
 import GlobalStyle from './styles/global';
 
+import AppProvider from './hooks';
+
+import Routes from './routes';
+
 const App: React.FC = () => {
   return (
-    <>
-      <SignIn />
+    <Router>
+      <AppProvider>
+        <Routes />
+      </AppProvider>
+
       <GlobalStyle />
-    </>
+    </Router>
   );
 };
 
