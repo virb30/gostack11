74f61eca480a5021b233744f25a78ef000a0c771
Vinicius Boscoa <virb30@gmail.com>
2020-04-25 01:13:32 -0300

    finished module 02 from react-native class

--- nivel_03/mobile/appgobarber/src/components/Input/index.tsx
+++ nivel_03/mobile/appgobarber/src/components/Input/index.tsx
@@ -68,7 +68,7 @@ const Input: React.RefForwardingComponent<InputRef, InputProps> = (
   }, [fieldName, registerField]);
 
   return (
-    <Container isFocused={isFocused}>
+    <Container isFocused={isFocused} hasError={!!error}>
       <Icon
         name={icon}
         size={20}
